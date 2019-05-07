c3f1f2c6fcecf6250d2fa7655cce26a3edd2d381
Volodymyr Kostrytsia <50015766+vkostrytsia@users.noreply.github.com>
2019-05-07 11:15:22 +0300

    Update psbuild.ps1

--- psbuild.ps1
+++ psbuild.ps1
@@ -1,3 +1,4 @@
-   $project_files = Get-ChildItem -Path "$(Build.SourcesDirectory)" -Include "*.csproj" -Recurse
-   $dotnet_path = "$env:ProgramFiles\dotnet\dotnet.exe"
-   $project_files | ForEach-Object -Process { & $dotnet_path build $_.FullName '--configuration' $(buildConfiguration) }
+PARAM ($(Build.SourcesDirectory), $(buildConfiguration))
+$project_files = Get-ChildItem -Path "$(Build.SourcesDirectory)" -Include "*.csproj" -Recurse
+$dotnet_path = "$env:ProgramFiles\dotnet\dotnet.exe"
+$project_files | ForEach-Object -Process { & $dotnet_path build $_.FullName '--configuration' $(buildConfiguration) }
